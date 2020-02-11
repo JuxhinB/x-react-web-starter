2ff5508620166925171fccec166ab1b6a3c9f767
Juxhin Bleta <juxhin@newmedia.al>
2020-02-11 12:19:26 +0100

    small fix

--- src/container/register/RegisterScreen.tsx
+++ src/container/register/RegisterScreen.tsx
@@ -6,7 +6,7 @@ interface RegisterScreenProps {}
 function RegisterScreen() {
   return (
     <GeneralLayout>
-      <div className={'p-16 bg-red-600'}></div>
+      <div className={''}></div>
     </GeneralLayout>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./theme/style/index.scss";
+import "./theme/style/_main_index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
